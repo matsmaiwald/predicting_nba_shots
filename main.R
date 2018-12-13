7c45b1512ee59468f4153a93bb6f7894f67e7a1b
matsmaiwald <mats.maiwald@gmail.com>
2018-12-13 23:04:02 +0100

    Added square terms to numerical regressors.

--- main.R
+++ main.R
@@ -29,7 +29,7 @@ df_clean <- df_raw %>%
             final_margin = FINAL_MARGIN,
             shot_number = SHOT_NUMBER,
             period = PERIOD,
-            game_clock = GAME_CLOCK,
+            game_clock = as.numeric(GAME_CLOCK),
             shot_clock = SHOT_CLOCK,
             dribbles = DRIBBLES,
             touch_time = TOUCH_TIME,
@@ -41,23 +41,37 @@ df_clean <- df_raw %>%
             closest_defender_dist = CLOSE_DEF_DIST,
             pts = PTS,
             player_name = as.factor(player_name),
-            player_id = player_id) %>% 
+            player_id = player_id) %>%
   drop_na() %>% 
   select(-game_id, -matchup, -closest_defender_id, -player_id, -pts) %>% 
-  select(-closest_defender, -player_name)
+  select(-closest_defender, -player_name) %>% 
+  select(fgm, everything())
+
+# Adding square terms
+df_clean_2 <- df_clean %>%
+  mutate(final_margin_2 = final_margin^2,
+         shot_number_2 = shot_number^2,
+         game_clock_2 = game_clock^2,
+         shot_clock_2 = shot_clock^2,
+         dribbles_2 = dribbles^2,
+         touch_time_2 = touch_time^2,
+         shot_dist_2 = shot_dist^2,
+         closest_defender_dist_2 = closest_defender_dist^2,
+         )
+
 
 # Data exploration
-skim(df_clean)
-correlations <- cor(df_clean %>% select(-game_clock, -fgm))
+skim(df_clean_2)
+correlations <- cor(df_clean_2 %>% select(-fgm))
 corrplot(correlations, order = "hclust")
 
 # check for variables with very low variance
-nearZeroVar(df_clean)
+nearZeroVar(df_clean_2)
 
 # pre-process the data ---------------------------------------------------------
 set.seed(111)
 # stratified random split of the data
-df <- df_clean[1:10000, ] # only look at part of the data for exploratory analysis
+df <- df_clean_2[1:10000, ] # only look at part of the data for exploratory analysis
 in_training <- createDataPartition(y = df$fgm, p = 0.8, list = FALSE)
 df_train <- df[in_training, ]
 df_test <- df[-in_training,]
@@ -95,15 +109,15 @@ fitted_models[["lasso"]] <- train(model_spec, data = df_train,
 model_predictions[["lasso"]] <- predict.train(fitted_models[["lasso"]], df_test)
 toc()
 
-# knn
-tic()
-fitted_models[["knn"]] <- train(model_spec, data = df_train, 
-                                method = "knn", 
-                                preProc = pre_proc_options, 
-                                trControl = fit_control,
-                                tuneGrid = grids[["knn"]])
-model_predictions[["knn"]] <- predict.train(fitted_models[["knn"]], df_test)
-toc()
+# # knn
+# tic()
+# fitted_models[["knn"]] <- train(model_spec, data = df_train, 
+#                                 method = "knn", 
+#                                 preProc = pre_proc_options, 
+#                                 trControl = fit_control,
+#                                 tuneGrid = grids[["knn"]])
+# model_predictions[["knn"]] <- predict.train(fitted_models[["knn"]], df_test)
+# toc()
 
 # evaluate the models ----------------------------------------------------------
 confusionMatrix(model_predictions[["ridge"]],df_test$fgm)
@@ -118,9 +132,9 @@ plot(fitted_models[["lasso"]])
 fitted_models[["lasso"]]
 predictors(fitted_models[["lasso"]])
 
-confusionMatrix(model_predictions[["knn"]],df_test$fgm)
-#varImp(fitted_models$knn) not implicable to knn
-plot(fitted_models[["knn"]])
-fitted_models[["knn"]]
-predictors(fitted_models[["knn"]])
+# confusionMatrix(model_predictions[["knn"]],df_test$fgm)
+# #varImp(fitted_models$knn) not implicable to knn
+# plot(fitted_models[["knn"]])
+# fitted_models[["knn"]]
+# predictors(fitted_models[["knn"]])
 
