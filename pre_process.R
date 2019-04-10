b9bf5359ddc363df6f0fee10f5b2f729fc7ed645
matsmaiwald <mats.maiwald@gmail.com>
2019-04-10 22:48:04 +0200

    Graphs with histogram and beta dist for each distance

--- pre_process.R
+++ pre_process.R
@@ -44,38 +44,78 @@ df_clean <- df_raw %>%
   select(shot_result, everything())
 rm(df_raw)
 
+shot_dist_list <-  
+  list(
+    close = c(0, 7),
+    semi_close = c(7, 14),
+    semi_far = c(14, 23.75),
+    far = c(23.75, 99)
+  )
+
+df_clean[["shot_dist_cat"]] <- NA_character_
+for (distance in names(shot_dist_list)) {
+  print(distance)
+  df_clean[["shot_dist_cat"]] <- 
+    if_else(
+      df_clean$shot_dist >= shot_dist_list[[distance]][1] & 
+        df_clean$shot_dist <= shot_dist_list[[distance]][2], 
+      distance, 
+      df_clean[["shot_dist_cat"]] 
+    )
+}
+
 df_averages <- 
   df_clean %>% 
-  group_by(player_id, player_name) %>% 
+  group_by(player_id, player_name, shot_dist_cat) %>% 
   summarise(
     succesful_shots = sum(shot_result == "made"),
     attempts = n()
     ) %>%
   mutate(shot_pct = succesful_shots / attempts) %>% 
   ungroup() %>% 
-  filter(attempts > 300)
+  arrange(shot_dist_cat, desc(shot_pct))
+
 
 log_likelihood <- function(alpha, beta, attempts, successes) {
   -sum(VGAM::dbetabinom.ab(successes, attempts, alpha, beta, log = TRUE))
 }
 
-m <- mle(log_likelihood, 
-         start = list(alpha = 1, beta = 10), 
-         method = "L-BFGS-B",
-         fixed = list(successes = df_averages$succesful_shots, attempts = df_averages$attempts),
-         lower = c(0.0001, .1))
+estimate_alpha_beta <- function(df, successes_col, attempts_col) {
+  m <- mle(log_likelihood, 
+           start = list(alpha = 1, beta = 10), 
+           method = "L-BFGS-B",
+           fixed = list(successes = df[[successes_col]], attempts = df[[attempts_col]]),
+           lower = c(0.0001, .1))
+  
+  ab <- coef(m)
+  params_list <- list(alpha0 = ab[1], beta0 = ab[2])
+  params_list
+}
 
-ab <- coef(m)
-alpha0 <- ab[1]
-beta0 <- ab[2]
+beta_dist_params <- list()
+dfs <- list()
+for (dist_name in names(shot_dist_list)) {
+  dfs[[dist_name]] <- df_averages[df_averages$shot_dist_cat == dist_name,]
+  beta_dist_params[[dist_name]] <- estimate_alpha_beta(dfs[[dist_name]], "succesful_shots", "attempts")
+  
+  p0 = qplot(dfs[[dist_name]]$shot_pct, geom = 'blank') +
+    stat_function(fun = dbeta, aes(colour = 'Fitted beta distribution'), 
+                  args = list(shape1 = beta_dist_params[[dist_name]]$alpha0, 
+                              shape2 = beta_dist_params[[dist_name]]$beta0)
+                  ) +
+    geom_histogram(aes(y = ..density..), alpha = 0.4) +
+    #scale_colour_manual(name = 'Density', values = c('red', 'blue')) +
+    theme(legend.position = c(0.85, 0.85)) +
+    ggtitle(paste0("Histogram plus fitted beta distribution for ", 
+                   dist_name, 
+                   " distances"))
+  p0
+  fig_path <- paste0("./Figs/shot_distribution", dist_name, ".png") 
+  ggsave(fig_path)
+} 
 
 
-p0 = qplot(df_averages$shot_pct, geom = 'blank') +   
-  stat_function(fun = dbeta, aes(colour = 'Normal'), args = list(shape1 = alpha0, shape2 = beta0)) +                       
-  geom_histogram(aes(y = ..density..), alpha = 0.4) +                        
-  #scale_colour_manual(name = 'Density', values = c('red', 'blue')) + 
-  theme(legend.position = c(0.85, 0.85))
-p0
+
 
 
 # -----------------------------------------------------------------------------
